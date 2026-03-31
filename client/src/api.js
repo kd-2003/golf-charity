f4d02b84fc3088ab7d479c1cbdc36d40b89674c7
Kuldeep-Prajapati <kumarkuldeep56000@gmail.com>
2026-03-31 12:43:30 +0530

    Update api.js

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "https://golf-charity-backend-2xd5.onrender.com/api",
+  baseURL: "https://golf-charity-backend-2xd5.onrender.com/api",
 });
 
 export function setToken(token) {
